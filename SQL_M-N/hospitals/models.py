2dd24799d68f70e43ddc060d4a976dbddc2dfce3
holawan <asdf134652@gmail.com>
2022-04-18 11:34:28 +0900

    relation model

--- SQL_M-N/hospitals/models.py
+++ SQL_M-N/hospitals/models.py
@@ -9,9 +9,19 @@ class Doctor(models.Model):
 
 
 class Patient(models.Model):
-    # ManyToManyField - related_name 작성
-    doctors = models.ManyToManyField(Doctor, related_name='patients')
+    doctors = models.ManyToManyField(Doctor, through='Reservation')
     name = models.TextField()
 
     def __str__(self):
         return f'{self.pk}번 환자 {self.name}'
+
+
+class Reservation(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    symptom = models.TextField()
+    reserved_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.doctor.pk}번 의사의 {self.patient.pk}번 환자'
+
