b5a600f894d4d1f3ac877779454258f2f33ca153
holawan <asdf134652@gmail.com>
2022-06-15 00:48:45 +0900

    iNDEX 연습

---
+++ SQL_basic/index_practice.sql
@@ -0,0 +1,8 @@
+-- $ sqlite3 tutorial.sqlite3 
+-- $ .mode csv
+-- $ .import users.csv users_user
+-- SELECT * FROM users_user;
+-- drop TABLE users;
+-- CREATE INDEX rowid
+-- ON users_user (first_name);
+
